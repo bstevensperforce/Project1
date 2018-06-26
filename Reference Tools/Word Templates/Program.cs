44ff732b6bb5c7ffa9883bcd3640bf3cd6890945
bstevensperforce <40606081+bstevensperforce@users.noreply.github.com>
2018-06-26 16:25:10 -0400

    [IS - 4]
    [IS - 4]

---
+++ Reference Tools/Word Templates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SDKTestCSharp.ttsoap;
+
+namespace SDKTestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ttsoapcgi mySoap = new ttsoapcgi();
+	//Test
+            mySoap.Url = "http://localhost/Scripts/ttsoapcgi.exe";
+            CProject[] aproject = mySoap.getProjectList("administrator", "");
+
+            long LSession = mySoap.ProjectLogon(aproject[0], "administrator", "");
+
+
+
+        }
+    }
+}
